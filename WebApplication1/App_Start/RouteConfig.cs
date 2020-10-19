0023c36c002cc402c4075385ff4f34ed26c043d5
DUAN <a0955252322@gmail.com>
2020-10-19 22:04:45 +0800

    加入專案檔案。

---
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default2",
+                url: "{controller}/{action}/{name}/{numTimes}",
+                defaults: new { controller = "Hello", action = "Welcome", numTimes = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Hello", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
